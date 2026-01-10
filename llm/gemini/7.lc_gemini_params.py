46052bb91602aa03ee7787f32c3fec68cea8ba98
srikanthpragada <srikanthpragada@gmail.com>
2026-01-10 20:54:49 +0530

    10-JAN

--- llm/gemini/7.lc_gemini_params.py
+++ llm/gemini/7.lc_gemini_params.py
@@ -1,8 +1,7 @@
 from langchain.chat_models import init_chat_model
-from langchain_core.messages import HumanMessage
 model = init_chat_model("gemini-2.5-flash",
                         model_provider="google_genai",
-                        temperature=0.7,
+                        temperature=0.9,
                         max_output_tokens=300)
 response = model.invoke("Write a short story about Moon in 5 sentenses")
 print(response.content)
